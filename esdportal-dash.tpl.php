f254612c653194f3ede8d109cdd98b296951c7d0
Benjamin Chodoroff <ben@theworkdept.com>
2013-07-30 23:44:08 -0400

    refs #3069 - 32: mark updatable schools, include v/e links in tpl

--- esdportal-dash.tpl.php
+++ esdportal-dash.tpl.php
@@ -2,6 +2,11 @@
 <?php foreach ($schools as $nid => $data): ?>
 <dt><?php print l($data['name'], 'node/' . $data['school_profile_nid']); ?></dt>
 <dd>
+<?php print l('View profile', 'node/' . $data['school_profile_nid']); ?>
+<?php if ($data['updatable']): ?>
+ / <?php print l('Edit profile', 'node/' . $data['school_profile_nid'] . '/edit'); ?>
+<?php endif; ?>
+<br />
 <?php print $data['aside']; ?>
 </dd>
 <?php endforeach; ?>
