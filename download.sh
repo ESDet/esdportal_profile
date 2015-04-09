b6541af2f0a2b7c9c080642668aaef5cbf821f34
Benjamin Chodoroff <ben@theworkdept.com>
2015-04-09 15:26:22 -0400

    all ec term fields are nice

---
+++ download.sh
@@ -0,0 +1,11 @@
+# Download the data for the Ledger
+
+EC_WORKBOOK_BASE='https://docs.google.com/spreadsheets/d/1smYR13Ge7YSMgPBoLrfQn3nG2wNA8kKeT5m-x8ITVoA/export?format=csv&gid=';
+
+WGET=`which wget`
+
+DESTDIR=/tmp/migrate_ec
+
+mkdir -p $DESTDIR
+
+$WGET -O $DESTDIR/esd-ec-gsexport.csv "${EC_WORKBOOK_BASE}878101549"
