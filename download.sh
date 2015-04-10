4122c104d4dca428b2cd0dddc9d557572258ddd1
Benjamin Chodoroff <ben@theworkdept.com>
2015-04-10 11:02:21 -0400

    esd ec profiles

--- download.sh
+++ download.sh
@@ -1,6 +1,7 @@
 # Download the data for the Ledger
 
 EC_WORKBOOK_BASE='https://docs.google.com/spreadsheets/d/1smYR13Ge7YSMgPBoLrfQn3nG2wNA8kKeT5m-x8ITVoA/export?format=csv&gid=';
+EC_ESD_2015_BASE='https://docs.google.com/spreadsheets/d/1e7VHAaySfJuZtpaygi2kuKj0gdUquRgNqn8EQhMNXcg/export?format=csv&gid=';
 
 WGET=`which wget`
 
@@ -10,4 +11,8 @@ mkdir -p $DESTDIR
 
 $WGET -O $DESTDIR/esd-ec-gsexport.csv "${EC_WORKBOOK_BASE}878101549"
 
-head -11 $DESTDIR/esd-ec-gsexport.csv $DESTDIR/esd-ec-gsexport-test.csv
+head -11 $DESTDIR/esd-ec-gsexport.csv > $DESTDIR/esd-ec-gsexport-test.csv
+
+$WGET -O $DESTDIR/esd-ec-2015.csv "${EC_ESD_2015_BASE}1032706107"
+
+head -11 $DESTDIR/esd-ec-2015.csv > $DESTDIR/esd-ec-2015-test.csv
