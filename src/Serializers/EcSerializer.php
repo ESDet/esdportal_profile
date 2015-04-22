c42dc22f0662bd9300cf11e8d332ca5c475afaf3
Benjamin Chodoroff <ben@theworkdept.com>
2015-04-22 10:37:23 -0400

    start early childhood centers endpoint using jsonapi

---
+++ src/Serializers/EcSerializer.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\esdportal_api\Serializers\EcSerializer.
+ *
+ * Serializes early childhood taxonomy terms.
+ */
+
+namespace Drupal\esdportal_api\Serializers;
+
+use Tobscure\JsonApi\SerializerAbstract;
+
+class EcSerializer extends SerializerAbstract {
+  protected $type = 'ecs';
+
+  protected function attributes($ec_term) {
+    return $ec_term;
+  }
+}
