b4538f18e37ba17303d579f461d318cead54975b
gl2748 <imaitland@gmail.com>
2015-09-24 18:01:47 -0400

    import function still not working

--- teacher_import.php
+++ teacher_import.php
@@ -1,9 +1,9 @@
 <?php
 
-$row = 1;
 if (($handle = fopen("/home/gl2748/Dev/ESD6/profiles/esdportal_profile/modules/custom/esdportal_e3/initial_teacher_import.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
 //        print_r($data);
+//        print_r($data[1]);
         teacherimport($data);
         }
     }
@@ -11,7 +11,9 @@ if (($handle = fopen("/home/gl2748/Dev/ESD6/profiles/esdportal_profile/modules/c
 
 function teacherimport() {
   $newteacher = entity_create('esdportal_contact', array('type' =>'teacher'));
-  $newteacher->firstname = $data[0];
-//  $newteacher->save(); 
- entity_save('esdportal_contact', $newteacher);
+//  $newteacher->uid = '1';
+  $wrapper = entity_metadata_wrapper('esdportal_contact', $newteacher);
+  $wrapper->firstname = $data[0];
+  $wrapper->field_email = $data[2];
+  $wrapper->save();
 }
