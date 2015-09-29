cac5c524dc0ca92baac1709d50642f528fcbc12f
Benjamin Chodoroff <bc@thermitic.net>
2015-09-29 08:16:47 -0400

    fix importer to work with phone numbers in addressfield

--- teacher_import.php
+++ teacher_import.php
@@ -2,9 +2,10 @@
 
 /**
  * @file
+ * CSV importer for teacher bundle contact entities.
  */
 
-$csv_path = "/home/gl2748/Dev/ESD6/profiles/esdportal_profile/modules/custom/esdportal_e3/initial_teacher_import.csv";
+$csv_path = "/home/bc/Downloads/initial_teacher_import.csv";
 
 if (($handle = fopen($csv_path, "r")) !== FALSE) {
   while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
@@ -14,16 +15,24 @@ if (($handle = fopen($csv_path, "r")) !== FALSE) {
 fclose($handle);
 
 /**
+ * Imports a single teacher record from a CSV row.
  *
+ * @param array $data
+ *   Four-element array from CSV.
  */
 function teacherimport($data) {
   $newteacher = entity_create('e3_contact', array('type' => 'teacher'));
+
   $newteacher->uid = '1';
   $newteacher->firstname = $data[0];
   $newteacher->lastname = $data[1];
   $fullname = implode(' ', array($data[0], $data[1]));
-  $newteacher->title = $fullname;
-  $newteacher->field_teacher_phone = array(LANGUAGE_NONE => array(0 => array('value' => $data[3])));
+  $newteacher->fullname = $fullname;
   $newteacher->field_email = array(LANGUAGE_NONE => array(0 => array('value' => $data[2])));
+
+  $wrapper = entity_metadata_wrapper('e3_contact', $newteacher);
+
+  $wrapper->field_contact_addy->phone_number = $data[3];
+
   entity_save('e3_contact', $newteacher);
 }
